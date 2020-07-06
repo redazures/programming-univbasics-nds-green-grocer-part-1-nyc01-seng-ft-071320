0a4fa1d641947edc0bbe91bc6c825d9f68ff7918
james wu <jwu_nyc@yahoo.com>
2020-07-06 00:26:43 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -15,7 +15,7 @@ end
 def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   collection.each do |k,v|
-    p k
+    p k[:item]
     p v
   end
   # Consult README for inputs and outputs
