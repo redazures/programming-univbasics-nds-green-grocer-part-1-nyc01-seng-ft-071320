85e714208bbc3a85612a32e0e5d265f485ba4bfb
james wu <jwu_nyc@yahoo.com>
2020-07-06 00:35:20 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -27,7 +27,7 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-  p cart
+  cart
 end
 
 #p find_item_by_name_in_collection("CANNED BEANS",grocery_shelf)
