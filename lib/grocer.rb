1979d0ced447166e9695ec6e9900c81362d1afeb
james wu <jwu_nyc@yahoo.com>
2020-07-06 02:00:56 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -39,9 +39,7 @@ def consolidate_cart(cart)
     end
     new_cart<< k.merge(:count=>count) if find_item_by_name_in_collection(k[:item],new_cart) == nil
   end
-  # new_cart.each do|k|
-  #   p k
-  # end
+new_cart
 end
 
 #p find_item_by_name_in_collection("CANNED BEANS",grocery_shelf)
