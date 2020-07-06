721c39381b28431ea29706e2e26f9c2203177099
james wu <jwu_nyc@yahoo.com>
2020-07-06 00:16:35 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -15,7 +15,7 @@ end
 def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   collection.each do |item|
-    p info
+    p item
   end
   # Consult README for inputs and outputs
 
