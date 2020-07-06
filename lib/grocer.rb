fe4fb1f3973863180536c9a97c45dc42d27cd198
james wu <jwu_nyc@yahoo.com>
2020-07-06 00:51:51 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -36,5 +36,5 @@ def consolidate_cart(cart)
 end
 
 #p find_item_by_name_in_collection("CANNED BEANS",grocery_shelf)
-p consolidate_cart(cart)
-cart.include?'AVOCADO'
+#p consolidate_cart(cart)
+p cart.include?'AVOCADO'
