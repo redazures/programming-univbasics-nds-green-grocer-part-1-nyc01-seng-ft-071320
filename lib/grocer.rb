cc83475ff4b197e1fb8fd6ce1964af971e2ceea1
james wu <jwu_nyc@yahoo.com>
2020-07-06 00:41:53 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -28,6 +28,9 @@ def consolidate_cart(cart)
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   new_cart=[]
+  cart.each do |k|
+    new_cart<<k
+  end
   cart
 end
 
